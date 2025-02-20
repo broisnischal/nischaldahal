56e8606fd288eb0f77b21dcbfe7753020780fd59
broisnischal <info@nischal-dahal.com.np>
2025-02-20 15:50:17 +0545

    feat:blog-typed-env-react-router

--- app/misc/env.common.ts
+++ app/misc/env.common.ts
@@ -1,5 +1,5 @@
-import { z } from "zod";
-import { camelKeys } from "string-ts";
+import { camelKeys } from 'string-ts'
+import { z } from 'zod'
 
 // Define the environment schema using ArkType.
 
@@ -12,51 +12,51 @@ import { camelKeys } from "string-ts";
 // const getArkEnv = makeTypedEnvironment((d) => envArkSchema.assert(d));
 
 const publicEnvSchema = z.object({
-  GOOGLE_MAPS_API_KEY: z.string(),
-  STRIPE_PUBLIC_KEY: z.string(),
-});
+	GOOGLE_MAPS_API_KEY: z.string(),
+	STRIPE_PUBLIC_KEY: z.string(),
+})
 
 const privateEnvSchema = z.object({
-  STRIPE_SECRET_KEY: z.string().optional(),
-});
+	STRIPE_SECRET_KEY: z.string().optional(),
+})
 
 function makeTypedEnv<T>(schema: { parse: (data: unknown) => T }) {
-  return (args: Record<string, unknown>) => camelKeys(schema.parse(args));
+	return (args: Record<string, unknown>) => camelKeys(schema.parse(args))
 }
 
 export function getPublicEnvExpose() {
-  if (typeof window == "undefined") {
-    return getPublicEnvFrom(process.env);
-  }
+	if (typeof window == 'undefined') {
+		return getPublicEnvFrom(process.env)
+	}
 }
 
-const getPublicEnvFrom = makeTypedEnv(publicEnvSchema);
+const getPublicEnvFrom = makeTypedEnv(publicEnvSchema)
 
 function getPrivateEnv() {
-  return privateEnvSchema.parse(process.env);
+	return privateEnvSchema.parse(process.env)
 }
 
-const getPrivateEnvFrom = makeTypedEnv(privateEnvSchema);
+const getPrivateEnvFrom = makeTypedEnv(privateEnvSchema)
 
-export type PublicEnv = ReturnType<typeof getPublicEnvFrom>;
-export type PrivateEnv = ReturnType<typeof getPrivateEnvFrom>;
+export type PublicEnv = ReturnType<typeof getPublicEnvFrom>
+export type PrivateEnv = ReturnType<typeof getPrivateEnvFrom>
 
 function getPublicEnv() {
-  if (typeof window == "undefined") {
-    return getPublicEnvFrom(process.env);
-  }
+	if (typeof window == 'undefined') {
+		return getPublicEnvFrom(process.env)
+	}
 
-  if (!window.PUBLIC_ENV) {
-    throw new Error("PUBLIC_ENV is not defined, Not exposed!");
-  }
+	if (!window.PUBLIC_ENV) {
+		throw new Error('PUBLIC_ENV is not defined, Not exposed!')
+	}
 
-  return getPublicEnvFrom(window.PUBLIC_ENV);
+	return getPublicEnvFrom(window.PUBLIC_ENV)
 }
 
-export { publicEnvSchema, makeTypedEnv, getPublicEnv };
+export { publicEnvSchema, makeTypedEnv, getPublicEnv }
 
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends PublicEnv, PrivateEnv {}
-  }
+	namespace NodeJS {
+		interface ProcessEnv extends PublicEnv, PrivateEnv {}
+	}
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,121 +1,119 @@
-import "#app/tailwind.css";
-import clsx from "clsx";
+import '#app/tailwind.css'
+import clsx from 'clsx'
 import {
-  data,
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-  useRouteError,
-} from "react-router";
-import type { Route } from "./+types/root";
-import Navbar from "./components/common/navbar";
-import { ScriptDangerously } from "./lib/utils";
-import { getPublicEnv } from "./misc/env.common";
-import { useTheme } from "./routes/resources/theme-switch";
-import { ClientHintCheck, getHints } from "./utils/client-hints";
-import { getTheme, type Theme } from "./utils/theme.server";
+	data,
+	isRouteErrorResponse,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+	useRouteError,
+} from 'react-router'
+import { type Route } from './+types/root'
+import Navbar from './components/common/navbar'
+import { PublicEnv, ScriptDangerously } from './lib/utils'
+import { getPublicEnv } from './misc/env.common'
+import { useTheme } from './routes/resources/theme-switch'
+import { ClientHintCheck, getHints } from './utils/client-hints'
+import { getTheme, type Theme } from './utils/theme.server'
 
 export async function loader({ request }: Route.LoaderArgs) {
-  return data({
-    requestInfo: {
-      hints: getHints(request),
-      path: new URL(request.url).pathname,
-      userPrefs: {
-        theme: getTheme(request),
-      },
-    },
-    publicEnv: getPublicEnv(),
-  });
+	return data({
+		requestInfo: {
+			hints: getHints(request),
+			path: new URL(request.url).pathname,
+			userPrefs: {
+				theme: getTheme(request),
+			},
+		},
+		publicEnv: getPublicEnv(),
+	})
 }
 
 function Document({
-  children,
-  theme = "light",
-  loaderData,
+	children,
+	theme = 'light',
+	loaderData,
 }: {
-  children: React.ReactNode;
-  loaderData: Route.ComponentProps["loaderData"];
-  theme?: Theme;
+	children: React.ReactNode
+	loaderData: Route.ComponentProps['loaderData']
+	theme?: Theme
 }) {
-  return (
-    <html lang="en" className={clsx(theme)} data-theme={theme}>
-      <head>
-        <ClientHintCheck nonce={new Date().toString()} />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="color-scheme"
-          content={theme === "light" ? "light" : "dark"}
-        />
-        <meta name="MobileOptimized" content="320" />
-        <meta name="pagename" content="Nischal Dahal" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <Meta />
-        <Links />
-      </head>
-      <body className="">
-        <Layout>
-          <Navbar theme={theme} />
-          {children}
-        </Layout>
-        <ScrollRestoration
-          getKey={(location) => {
-            return location.pathname;
-          }}
-        />
-        <Scripts />
-        <ScriptDangerously
-          html={`window.PUBLIC_ENV = ${JSON.stringify(loaderData.publicEnv)}`}
-        />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={clsx(theme)} data-theme={theme}>
+			<head>
+				<ClientHintCheck nonce={new Date().toString()} />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="color-scheme"
+					content={theme === 'light' ? 'light' : 'dark'}
+				/>
+				<meta name="MobileOptimized" content="320" />
+				<meta name="pagename" content="Nischal Dahal" />
+				<meta name="mobile-web-app-capable" content="yes" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="">
+				<Layout>
+					<Navbar theme={theme} />
+					{children}
+				</Layout>
+				<ScrollRestoration
+					getKey={(location) => {
+						return location.pathname
+					}}
+				/>
+				<Scripts />
+				<PublicEnv {...loaderData.publicEnv} />
+			</body>
+		</html>
+	)
 }
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-3xl m-auto py-10 p-4 sm:px-0">{children}</div>;
+	return <div className="m-auto max-w-3xl p-4 py-10 sm:px-0">{children}</div>
 }
 
 /**Root App */
 export default function App({ loaderData }: Route.ComponentProps) {
-  const theme = useTheme();
+	const theme = useTheme()
 
-  return (
-    <Document loaderData={loaderData} theme={theme}>
-      <Outlet />
-    </Document>
-  );
+	return (
+		<Document loaderData={loaderData} theme={theme}>
+			<Outlet />
+		</Document>
+	)
 }
 
 export function ErrorBoundary({}: Route.ErrorBoundaryProps) {
-  const theme = useTheme();
-  const loaderDataA = useLoaderData();
-  const error: any = useRouteError();
+	const theme = useTheme()
+	const loaderDataA = useLoaderData()
+	const error: any = useRouteError()
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <>
-        <h1>
-          {error.status} {error.statusText}
-        </h1>
-        <p>{error.data}</p> 
-      </>
-    );
-  }
+	if (isRouteErrorResponse(error)) {
+		return (
+			<>
+				<h1>
+					{error.status} {error.statusText}
+				</h1>
+				<p>{error.data}</p>
+			</>
+		)
+	}
 
-  return (
-    <>
-      <Document loaderData={loaderDataA} theme={theme}>
-        <Layout>
-          <h1 className="text-3xl font-bold">Opps Sorry!</h1>
-          <br />
-          <p>{error?.message ?? "Unknown error"}</p>
-        </Layout>
-      </Document>
-    </>
-  );
+	return (
+		<>
+			<Document loaderData={loaderDataA} theme={theme}>
+				<Layout>
+					<h1 className="text-3xl font-bold">Opps Sorry!</h1>
+					<br />
+					<p>{error?.message ?? 'Unknown error'}</p>
+				</Layout>
+			</Document>
+		</>
+	)
 }
