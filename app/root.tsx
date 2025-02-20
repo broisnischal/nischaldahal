d026d1170a3d7cc0968a4fbb452cb648f5482e20
broisnischal <info@nischal-dahal.com.np>
2025-02-20 14:38:32 +0545

    feat:impl-setup

--- app/root.tsx
+++ app/root.tsx
@@ -52,9 +52,9 @@ function Document({
           name="color-scheme"
           content={theme === "light" ? "light" : "dark"}
         />
-        {/* <meta name="MobileOptimized" content="320" /> */}
+        <meta name="MobileOptimized" content="320" />
         <meta name="pagename" content="Nischal Dahal" />
-        {/* <meta name="mobile-web-app-capable" content="yes" /> */}
+        <meta name="mobile-web-app-capable" content="yes" />
         <Meta />
         <Links />
       </head>
