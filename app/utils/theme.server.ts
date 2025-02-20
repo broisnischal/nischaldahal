4c0861d7866e5b1af09dd7ac941dbf32e04591ef
broisnischal <info@nischal-dahal.com.np>
2025-02-20 16:05:09 +0545

    feat:blog-typed-env-react-router

--- app/utils/theme.server.ts
+++ app/utils/theme.server.ts
@@ -1,22 +1,20 @@
-import * as cookie from "cookie";
+import * as cookie from 'cookie'
 
-export type Theme = "light" | "dark";
+export type Theme = 'light' | 'dark'
 
-const cookieName = "en_theme";
+const cookieName = 'en_theme'
 
-export function setTheme(theme: Theme | "system") {
-  if (theme === "system") {
-    return cookie.serialize(cookieName, "", { path: "/", maxAge: -1 });
-  } else {
-    return cookie.serialize(cookieName, theme, { path: "/", maxAge: 31536000 });
-  }
+export function setTheme(theme: Theme | 'system') {
+	if (theme === 'system') {
+		return cookie.serialize(cookieName, '', { path: '/', maxAge: -1 })
+	} else {
+		return cookie.serialize(cookieName, theme, { path: '/', maxAge: 31536000 })
+	}
 }
 
 export function getTheme(request: Request): Theme | null {
-  const cookieHeader = request.headers.get("cookie");
-  const parsed = cookieHeader
-    ? cookie.parse(cookieHeader)[cookieName]
-    : "light";
-  if (parsed === "light" || parsed === "dark") return parsed;
-  return null;
+	const cookieHeader = request.headers.get('cookie')
+	const parsed = cookieHeader ? cookie.parse(cookieHeader)[cookieName] : 'light'
+	if (parsed === 'light' || parsed === 'dark') return parsed
+	return null
 }
