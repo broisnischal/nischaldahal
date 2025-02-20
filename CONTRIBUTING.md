3216c2e501709ea861a732e787d56dce45eb1bc5
broisnischal <info@nischal-dahal.com.np>
2025-02-20 14:49:21 +0545

    feat:fly-deploy-change

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+# Contributing
+
+Thanks for your willingness to contribute! Please make sure to check with me before working in something.
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwind from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { reactRouter } from '@react-router/dev/vite'
+import tailwind from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+
+const MODE = process.env.NODE_ENV
 
 export default defineConfig({
-  plugins: [
-    reactRouter(),
-    tailwind(),
-  ],
-  server: {
-    allowedHosts: ['.snehaa.store']
-  }
-});
+	plugins: [reactRouter(), tailwind()],
+	server: {
+		allowedHosts: ['.snehaa.store'],
+		watch: {},
+	},
+	build: {
+		cssMinify: MODE === 'production',
+		sourcemap: true,
+		assetsInlineLimit: (source: string) => {
+			if (
+				source.endsWith('sprite.svg') ||
+				source.endsWith('favicon.svg') ||
+				source.endsWith('apple-touch-icon.png')
+			) {
+				return false
+			}
+		},
+	},
+})
