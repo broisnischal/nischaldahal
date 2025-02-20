330935c271cb856a06875b7f9706df24eb54920c
broisnischal <info@nischal-dahal.com.np>
2025-02-20 16:06:58 +0545

    feat:blog-typed-env-react-router

--- app/.server/content.server.ts
+++ app/.server/content.server.ts
@@ -1,94 +1,90 @@
-import { readdirSync, readFileSync } from "fs";
-import { bundleMDX } from "mdx-bundler";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode, {
-  type Theme as RehypeTheme, 
-} from "rehype-pretty-code"; 
-import { getTheme } from "../utils/theme.server";
-import matter from "gray-matter";
-
+import { readdirSync, readFileSync } from 'fs'
+import matter from 'gray-matter'
+import { bundleMDX } from 'mdx-bundler'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrettyCode, { type Theme as RehypeTheme } from 'rehype-pretty-code'
+import { getTheme } from '../utils/theme.server'
 
 export type Article = {
-  title: string;
-  category: string;
-  writtenAt: string;
-};
+	title: string
+	category: string
+	writtenAt: string
+}
 
 export async function bundlePost(slug: string, request: Request) {
-  const path = `${process.cwd()}/app/contents/${slug}`;
-  const theme = getTheme(request);
+	const path = `${process.cwd()}/app/contents/${slug}`
+	const theme = getTheme(request)
 
-  // github-dark
-  let rehypeTheme: RehypeTheme = theme === "dark" ? "dark-plus" : "min-light";
+	// github-dark
+	let rehypeTheme: RehypeTheme = theme === 'dark' ? 'dark-plus' : 'min-light'
 
-  return await bundleMDX({
-    file: `${path}/page.mdx`,
-    cwd: path,
-
-    esbuildOptions: (options) => {
-      // Configuration to allow image loading
-      // https://github.com/kentcdodds/mdx-bundler#image-bundling
-      options.loader = {
-        ...options.loader,
-        ".png": "dataurl",
-        ".jpg": "dataurl",
-        ".svg": "dataurl",
-        ".gif": "dataurl",
-      };
-      return options;
-    },
-    mdxOptions(options, frontmatter: Article) {
-      options.rehypePlugins = [
-        ...(options.rehypePlugins ?? []),
-        [
-          rehypePrettyCode,
-          {
-            ...(theme !== null ? { theme: rehypeTheme } : {}),
-          },
-        ],
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ["anchor"],
-            },
-          },
-        ],
-      ];
-      return options;
-    },
-  });
+	return await bundleMDX({
+		file: `${path}/page.mdx`,
+		cwd: path,
+		esbuildOptions: (options) => {
+			// Configuration to allow image loading
+			// https://github.com/kentcdodds/mdx-bundler#image-bundling
+			options.loader = {
+				...options.loader,
+				'.png': 'dataurl',
+				'.jpg': 'dataurl',
+				'.svg': 'dataurl',
+				'.gif': 'dataurl',
+			}
+			return options
+		},
+		mdxOptions(options, frontmatter: Article) {
+			options.rehypePlugins = [
+				...(options.rehypePlugins ?? []),
+				[
+					rehypePrettyCode,
+					{
+						...(theme !== null ? { theme: rehypeTheme } : {}),
+					},
+				],
+				[
+					rehypeAutolinkHeadings,
+					{
+						properties: {
+							className: ['anchor'],
+						},
+					},
+				],
+			]
+			return options
+		},
+	})
 }
 
 export async function listAllArticles(request: Request) {
-  // const dirs = getAllArticlesSlug();
-  // const articles = await Promise.all(
-  //   dirs.map(async (slug) => {
-  //     const { frontmatter } = await bundlePost(slug, request);
-  //     return { slug, ...frontmatter } as Article & { slug: string };
-  //   })
-  // );
-  // articles.sort((a, b) => (a.writtenAt < b.writtenAt ? 1 : -1));
-  // return articles;
-  const slugs = getAllArticlesSlug();
-  const articles = slugs.map((slug) => {
-    const frontmatter = getArticleMetadata(slug);
-    return { slug, ...frontmatter } as Article & { slug: string };
-  });
+	// const dirs = getAllArticlesSlug();
+	// const articles = await Promise.all(
+	//   dirs.map(async (slug) => {
+	//     const { frontmatter } = await bundlePost(slug, request);
+	//     return { slug, ...frontmatter } as Article & { slug: string };
+	//   })
+	// );
+	// articles.sort((a, b) => (a.writtenAt < b.writtenAt ? 1 : -1));
+	// return articles;
+	const slugs = getAllArticlesSlug()
+	const articles = slugs.map((slug) => {
+		const frontmatter = getArticleMetadata(slug)
+		return { slug, ...frontmatter } as Article & { slug: string }
+	})
 
-  articles.sort((a, b) => (a.writtenAt < b.writtenAt ? 1 : -1));
-  return articles;
+	articles.sort((a, b) => (a.writtenAt < b.writtenAt ? 1 : -1))
+	return articles
 }
 
 function getArticleMetadata(slug: string): Article {
-  const fileContent = readFileSync(
-    `${process.cwd()}/app/contents/${slug}/page.mdx`,
-    "utf-8"
-  );
-  const { data } = matter(fileContent);
-  return data as Article;
+	const fileContent = readFileSync(
+		`${process.cwd()}/app/contents/${slug}/page.mdx`,
+		'utf-8',
+	)
+	const { data } = matter(fileContent)
+	return data as Article
 }
 
 export function getAllArticlesSlug() {
-  return readdirSync(`${process.cwd()}/app/contents`);
+	return readdirSync(`${process.cwd()}/app/contents`)
 }
